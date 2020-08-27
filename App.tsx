665cca1ed18f6a476353f2d80dc153993e6eae60
DevWhkang <DevWhkang>
2020-08-27 17:38:38 +0900

    🎉 Initialize Project
    Initialize Project

---
+++ App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => (
+  <View>
+    <Text>
+      Hello World!
+    </Text>
+  </View>
+);
+
+export default App;
