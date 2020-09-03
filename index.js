2825e0fc33b50df6bab9dd5b82429e8bcbe6ce0e
DevWhkang <DevWhkang>
2020-09-03 23:14:14 +0900

    ♻️  Refactor CheckList Screen to MobX, Firebase
    resolve #20

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-/* global __DEV__ */
-
 import { AppRegistry } from 'react-native';
 import App from './App.tsx';
 import { name as appName } from './app.json';
-
-if (__DEV__) {
-  import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
-}
 
 AppRegistry.registerComponent(appName, () => App);
