6c8b313baafeb53b85d39180b658a4d5b436caba
Yim Jinsung <genuinenessss@gmail.com>
2020-09-10 13:49:57 +0900

    ♻️ Refactor HeaderTopBack

--- src/view/components/MyInfo/HeaderTopBack.tsx
+++ src/view/components/MyInfo/HeaderTopBack.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import styled from '@emotion/native';
+import { useNavigation } from '@react-navigation/native';
 
 const HeaderTopWrapper = styled.TouchableOpacity`
   padding: 10px;
@@ -10,15 +11,20 @@ const HeaderTopWrapper = styled.TouchableOpacity`
   flex-direction: row;
 `;
 
-const HeaderTopBack = (): JSX.Element => (
-  <HeaderTopWrapper>
-    <FontAwesomeIcon
-      color="#fc8a3d"
-      size={30}
-      icon={faChevronLeft}
-    />
+const HeaderTopBack = (): JSX.Element => {
+  const navigation = useNavigation();
+  return (
+    <HeaderTopWrapper
+      onPress={() => navigation.goBack()}
+    >
+      <FontAwesomeIcon
+        color="#fc8a3d"
+        size={30}
+        icon={faChevronLeft}
+      />
 
-  </HeaderTopWrapper>
-);
+    </HeaderTopWrapper>
+  );
+};
 
 export default HeaderTopBack;
