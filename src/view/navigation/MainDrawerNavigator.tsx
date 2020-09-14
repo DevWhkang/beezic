17693479a818b37e5e66e51f3b43937c3f652ab5
Yim Jinsung <genuinenessss@gmail.com>
2020-09-14 16:33:06 +0900

    ✨ Add darwer navigation feature

---
+++ src/view/navigation/MainDrawerNavigator.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import MainMenu from '../screens/MainMenuBar';
+import MainStackNavigator from './MainStackNavigator';
+import MyInfoStackNavigator from './MyInfoNavigator';
+
+const Drawer = createDrawerNavigator();
+
+const MainDrawerNavigator = ():JSX.Element => (
+  <Drawer.Navigator initialRouteName="MainStackNavigator" drawerContent={MainMenu}>
+    <Drawer.Screen name="MainMenu" component={MainMenu} />
+    <Drawer.Screen name="MainStackNavigator" component={MainStackNavigator} />
+  </Drawer.Navigator>
+);
+
+export default MainDrawerNavigator;
