f0b0469c341d97258ec6244ad0f014d73cba088c
jinyongp <dev.jinyongp@gmail.com>
2020-09-20 21:28:11 +0900

    🏷️ Update types of stores

--- src/viewModel/OAuthStore.ts
+++ src/viewModel/OAuthStore.ts
@@ -1,10 +1,10 @@
 import { observable } from 'mobx';
-import { OAuthStoreStates, UserCredentialTypes } from './@types/OAuthStore';
+import { OAuthStoreTypes } from './@types/OAuthStore';
 import UserStore from './UserStore';
 import ErrorStore from './ErrorStore';
 import OAuthModel from '../model/OAuthModel';
 
-const OAuthStore: OAuthStoreStates = observable({
+const OAuthStore: OAuthStoreTypes = observable({
   init() {
     try {
       OAuthModel.initGoogleConfigure();
@@ -15,7 +15,7 @@ const OAuthStore: OAuthStoreStates = observable({
 
   async google() {
     try {
-      const userCredential: UserCredentialTypes = await OAuthModel.onGoogleSignIn();
+      const userCredential = await OAuthModel.onGoogleSignIn();
       const { user } = userCredential;
       UserStore.set(user);
       console.log('Google Sign In: ', user);
@@ -26,7 +26,7 @@ const OAuthStore: OAuthStoreStates = observable({
 
   async facebook() {
     try {
-      const userCredential: UserCredentialTypes = await OAuthModel.onFaceBookSignIn();
+      const userCredential = await OAuthModel.onFaceBookSignIn();
       const { user } = userCredential;
       UserStore.set(user);
       console.log('Facebook Sign In: ', user);
